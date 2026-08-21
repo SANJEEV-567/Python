d376c5f4902950d121051bc77e5955831839a915
sanjeev kumar <sanjeevkumarxcg@gmail.com>
2026-08-21 22:34:00 +0530

    add about new file

---
+++ about_python.py
@@ -0,0 +1,2 @@
+# python is exciting language to learn and easy to understand
+print("Python is a programming language that allows us to write instructions for a computer in a simple and readable way.")

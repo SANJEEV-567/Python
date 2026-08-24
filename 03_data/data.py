cdc7173f963dc341824abd1dc604e0dc7dcaf095
sanjeev kumar <sanjeevkumarxcg@gmail.com>
2026-08-24 22:35:32 +0530

    add modified file

---
+++ 03_data/data.py
@@ -0,0 +1,56 @@
+#Student info
+student_name= "Sanjeev"
+student_age= 17
+student_height= 5.8
+is_student= True
+result= None
+print(student_name,student_age,student_height,is_student,result)
+
+a= 50
+b= 50.0
+c= "50"
+print(type(a))
+print(type(b))
+print(type(c))
+
+x= True
+y= "True"
+print(type(x))
+print(type(y))
+
+result=None
+marks="None"
+print(type(result))
+print(type(marks))
+
+value=7
+print(type(value))
+value= "57"
+print(type(value))
+
+#product
+name="hackode"
+quantity=100
+price=99.99
+is_available=True
+discount=None
+print(type(name))
+print(type(quantity))
+print(type(price))
+print(type(is_available))
+print(type(discount))
+
+days=10
+hours=10.0
+minutes="10"
+is_student=True
+is_student="True"
+result=None
+marks="None"
+print(type(days))
+print(type(hours))
+print(type(minutes))
+print(type(is_student))
+print(type(is_available))
+print(type(result))
+print(type(marks))

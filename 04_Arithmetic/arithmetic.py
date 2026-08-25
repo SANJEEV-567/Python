80c831a0b35a05499689ec39dbb423fc02522e69
sanjeev kumar <sanjeevkumarxcg@gmail.com>
2026-08-26 00:29:17 +0530

    add my new file

---
+++ 04_Arithmetic/arithmetic.py
@@ -0,0 +1,143 @@
+#Type1- Basic Arithmetic
+a=11
+b=5
+print(a+b)
+print(a-b)
+print(a/b)
+print(a*b)
+print(a//b)
+print(a%b)
+print(a**b)
+
+#Integer and Float
+a=17
+b=0.5
+print(a+b)
+print(a-b)
+print(a/b)
+print(a*b)
+print(a//b)
+print(a%b)
+print(a**b)
+
+# Student Marks
+maths=79
+sst=96
+science=91
+print(maths+sst+science)
+print(maths/3+sst/3+science/3)
+#Product Calculation
+price= 99
+quantity= 999
+print(price+quantity)
+
+#Even or Odd
+a=34
+b=2
+print(a%2)
+
+#Division and Floor Division
+d=-36
+b=23
+print(d/b)
+print(d//b)
+
+#Negative Number Operations
+a=-5
+b=-4
+print(a+b)
+print(a-b)
+print(a/b)
+print(a*b)
+print(a//b)
+print(a%b)
+
+#Subtraction Edge Cases
+positive=7
+negative=-6
+print(positive - positive)
+print(positive - negative)
+print(negative - positive)
+print(negative - negative)
+
+#Floor Division Edge Cases
+positive=40
+negative=-5
+print(positive // positive)
+print(negative // positive)
+print(positive // negative)
+print(negative // negative)
+
+#Modulus Edge Cases
+positive=9
+negative=-4
+print(positive % positive)
+print(negative % positive)
+print(positive % negative)
+print(negative % negative)
+
+#Part 4 — Operator Precedence
+#Task11
+print(10 + 5 * 2)
+print(20 - 4 / 2)
+print(10 + 20 / 5 * 2)
+print(2 + 3 * 4 ** 2)
+print(100 - 20 // 5)
+
+#Task 12 — Parentheses
+
+print(10 + 5 * 2)
+print((10 + 5) * 2)
+
+print(20 - 10 / 2)
+print((20 - 10) / 2)
+
+print(2 + 3 * 4)
+print((2 + 3) * 4)
+
+#Part 5 — Boolean Arithmetic
+#Task 13
+a=False
+b=True
+print(a+b)
+print(a-b)
+print(a/b)
+print(a*b)
+print(a//b)
+print(a%b)
+print(a**b)
+
+#Task 14
+print(True + 5)
+print(False + 5)
+print(True * 10)
+print(False * 10)
+print(True - 5)
+print(False - 5)
+
+#Part 6 — String Operations
+#Task 15
+first_name="sanjeev"
+last_name="kumar"
+print(first_name+last_name)
+
+#Task 16
+a= "3"
+print(a*3)
+
+#Task 17
+x="8"
+y="3"
+z=9
+print(x+y)
+print(x*z)
+#string + string string * integer  these two not allowed causes error
+
+#Task 18
+a = None
+b=8
+#all operations produced error
+
+#Task19
+
+#Task20

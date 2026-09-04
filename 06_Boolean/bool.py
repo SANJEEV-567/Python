c3fc7a6ca4edcef8de8a3d555ea0a2832b086886
sanjeev kumar <sanjeevkumarxcg@gmail.com>
2026-09-04 17:47:27 +0530

    add boolean File

---
+++ 06_Boolean/bool.py
@@ -0,0 +1,32 @@
+# 25
+number = 30
+print(10 < number and number < 50)  # True
+
+# 26
+number = 101
+print(10 > number or number > 100)  # True
+
+# 29
+print(bool(0))      # False
+print(bool(10))     # True
+print(bool(""))     # False
+print(bool("Hello")) # True
+print(bool(None))   # False
+
+# 30
+#data types
+print(type(0))        # <class 'int'>
+print(type(10))       # <class 'int'>
+print(type(""))       # <class 'str'>
+print(type("Python")) # <class 'str'>
+print(type(False))    # <class 'bool'>
+print(type(None))     # <class 'NoneType'>
+
+#truthiness of data types
+print(bool(0))
+print(bool(10))
+print(bool(""))
+print(bool("Python"))
+print(bool(False))
+print(bool(None))
+

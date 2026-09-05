2b769603c2a8e03c7cb809934fad5f35b2e4f129
SANJEEV-567 <sanjeevkumarxcg@gmail.com>
2026-09-05 09:38:06 +0530

    Uncomment input/output code in inputoutput.py

--- 07_Input_Output/inputoutput.py
+++ 07_Input_Output/inputoutput.py
@@ -1,22 +1,22 @@
-# # Q3
-# name= input("enter your name: ")
-# age= int(input("enter your age: "))
-# print(name,age)
+# Q3
+ name= input("enter your name: ")
+ age= int(input("enter your age: "))
+ print(name,age)
 
 # Q5
-# city= type(input("your city is: "))
-# print(city)
+ city= type(input("your city is: "))
+ print(city)
 
 # Q6
-# first_name= input("enter your first name: ")
-# last_name= input("enter your last name: ")
-# print(first_name,last_name)
+ first_name= input("enter your first name: ")
+ last_name= input("enter your last name: ")
+ print(first_name,last_name)
 
 #Q7
-# name= input("your name: ")
-# city=input("your city: ")
-# college= input("your college: ")
-# print(name,city,college)
+ name= input("your name: ")
+ city=input("your city: ")
+ college= input("your college: ")
+ print(name,city,college)
 
 #Q8
 first_name,last_name = map(input("your first and last name: ").split())

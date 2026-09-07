b4c3ce0e45cab0fe43fdcc6679c4ce0994deeea8
sanjeev kumar <sanjeevkumarxcg@gmail.com>
2026-09-07 09:40:25 +0530

    add both File

--- 07_Input_Output/inputoutput.py
+++ 07_Input_Output/inputoutput.py
@@ -19,7 +19,84 @@
 # print(name,city,college)
 
 #Q8
-first_name,last_name = map(input("your first and last name: ").split())
-print(first_name,last_name)
+# first_name,last_name = map(str,input("your first and last name: ").split())
+# print(first_name,last_name)
 
-a,b = map(int,input("Enter two numbers: ").split())
+#Q9
+# word2 = map(str,input("enter both words: ").split())
+# print(word1,word2)
+
+#Q10
+# movie,city,state= map(str,input("where are you visiting tommorrow: ").split())
+# print(movie,city,state)
+
+#Q11
+print(int("25"))
+
+#Q12
+print(float("25.5"))
+
+#Q13
+print(str(100))
+
+#Q14
+# age= type((int(input("enter your age: "))))
+# print(age)
+
+#Q15
+# price= type((float(input("enter product price: "))))
+# print(price)
+
+#Q16
+# a = int(input())
+# b = int(input())
+# print(a + b)
+
+#Q17
+# a = int(input("Enter first number: "))
+# b = int(input("Enter second number: "))
+# print(a + b)
+
+#Q18
+name = "Rahul"
+age = 20
+print(f"My name is {name} and I am {age} years old.")
+
+#Q19
+a = 10
+b = 20
+print(f"the sum of a and b is {a+b}")
+
+#Q20
+name = "Piyush"
+age = 19
+print(f"My name is {name} and I am {age} years old.")
+
+#Q21
+product_price=99.22345678
+print(f"{product_price:.2f}")
+
+#Q22
+#f-strings also provide a convenient way to control the number of decimal places.
+#Example- price = 99.5678
+# print(f"{price:.2f}")
+
+#Q23
+product_name= "harmony"
+price= 1999
+quantity= 10
+print(f"I have product named {product_name}and its price is {price} and quantity is {quantity}.")
+
+#Q24
+print("A", "B", "C")
+
+#Q25
+print("2026", "08", "19" , sep="-")
+
+#Q26
+print("Hello" , end=" ")
+print("World")
+# The end argument controls what is printed at the end.
+
+#Q27
+
---
+++ 08_Conditional_statements/conditional.py
@@ -0,0 +1,92 @@
+
+#Q1
+number= 11
+if number > 10:
+    print("Greater than 10")
+
+#Q2
+age = 20
+if age >= 18:
+    print("Adult")
+
+#Q3
+# number= input("Enter a Integer: ")
+# if age > 0:
+    # print("Positive")
+
+#Q4
+marks= 75
+if marks >= 45:
+    print("Pass")
+
+#Q5
+# number= int(input("Enter a Number: "))
+# if number == 0:
+    # print("Zero")
+
+#Q6
+# integer= int(input("Write a Interger: "))
+# if integer >= 0:
+#     print("Positive")
+# else:
+#     print("Not Positive")
+
+#Q7
+# age = 16
+
+# if age >= 18:
+#     print("Adult")
+# else:
+#     print("Minor")
+
+#Q8
+# number= input("Check Here Even , Odd: ")
+# if number==0:
+#     print("Even")
+# else:
+#     print("Odd")
+
+#Q9
+# marks= int(input("Enter Your Marks: "))
+# if marks >=40:
+#     print("Pass")
+# else:
+#     print("Fail")
+
+#Q10
+#Python provides elif, which means: Check another condition if the previous condition was False.
+# marks = int(input("Check Ypur Grade: "))
+# if marks >= 90:
+#     print("A")
+# elif marks >= 75:
+#     print("B")    
+# elif marks >= 60:
+#     print("c")
+# elif marks >= 40:
+#     print("D")
+# elif marks <40:
+#     print("F")    
+
+#Q12
+# number= int(input("check number type: "))
+# if number > 0:
+#     print("Positive")
+# elif number == 0:
+#     print("Zero")
+# else:
+#     print("Negative")
+
+# #Q13
+# day = int(input("Enter Day Index: "))
+
+# if number == 1:
+#     print("Monday")
+# elif number == 2:
+#     print("Tuesday")
+# elif number == 3:
+#     print("Wednesday")
+# elif number == 4:
+#     print("Thursday")
+# elif number== 5:
+#     print("Friday")    
+

19d7b2ae4913907ee10eaf7c9798a6efe8ce45e3
sanjeev kumar <sanjeevkumarxcg@gmail.com>
2026-09-08 17:45:50 +0530

    add ps file

---
+++ Problem_Solving/ps.py
@@ -0,0 +1,42 @@
+#09_problem_solving
+#Q1
+# integer= int(input("Check type of Integer: "))
+# if integer > 0:
+#     print("Positive")
+# elif integer <0:
+#     print("Negative")
+# elif integer ==0:
+#     print("Zero")
+
+#Q2
+# integer= int(input("Check Positive or Negative and Even or Odd: "))
+# if integer > 0 and integer%2==0:
+#     print("Positive Even")
+# elif integer >0 and integer%2!=0:
+#     print("Positive Odd")
+# elif integer<0 and integer%2==0:
+#     print("Negative Even")
+# elif integer<0 and integer%2!=0:
+#     print("Negative Odd")
+# elif integer ==0:
+#     print("Zero")    
+
+#Q3
+# first_number= int(input("Enter First Number: "))
+# second_number= int(input("Enter Second Number: "))
+# if first_number > second_number:
+#     print("First Number is Greater Than Second Number")
+# elif second_number > first_number:
+#     print("Second Number is Greater Than First Number")    
+# elif first_number == second_number :
+#     print("Both Are Equal Numbers")   
+ 
+#Q4
+first_number= int(input("Enter First Number: "))
+second_number= int(input("Enter Second Number: "))
+if first_number > second_number:
+    print("First Number is Greater Than Second Number")
+elif second_number > first_number:
+    print("Second Number is Greater Than First Number")    
+elif first_number == second_number :
+    print("Both Are Equal Numbers")           
